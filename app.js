f7063fed534f80c79592916df237cbfa61294f0d
Sanho LEE <stlsh0516@gmail.com>
2022-09-18 20:03:03 +0900

    Add and test cookie-parser middleware.

--- app.js
+++ app.js
@@ -1,42 +1,29 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+var express = require('express')
+var app = express()
+var path = require('path')
+var cookieparser = require('cookie-parser');
 
-const logger = require('morgan')
+var logger = require('morgan')
 const port = 4000
 
 // middlewares...
 app.use(logger('dev'))  // HTTP request logger.
-
+app.use(cookieparser())
 
 // built-in middlewares
-app.use(express.static(path.join(__dirname, 'public'))) // 'public' 아래에 있는 static 파일에 접근할 수 있게 된다.
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public'))) // 'public' 아래에 있는 static 파일에 접근할 수 있게 된다.
+
+
 
 app.get('/', (req,res) =>{
   res.send('Hello world!!! ')
+
+  console.log(req.cookies)
 })
 
 
 
-app.post('/sanho', (req,res) =>{
-  var myJson = {};
-  myJson.name = "sanho Lee";
-  myJson.prop = {
-    age : 33,
-    height : 184,
-    hobby : "programming"
-  }
-
-  req.body = {myJson}
-  console.log(req.body)
-  console.log(`${req.body.myJson.name} is ${req.body.myJson.prop.age} years old...`)
-
-  res.end()
-
-
-
-})
 app.listen(port, ()=>{
   console.log(`The app listening on port ${port}`)
 })
