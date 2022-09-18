317d0bddfdffb194754e68cc3f8842a81d1b3bb4
Sanho LEE <stlsh0516@gmail.com>
2022-09-18 20:39:10 +0900

    urlencoded middleware and add view engine, pug.

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var cookieparser = require('cookie-parser');
 var logger = require('morgan')
 const port = 4000
 
+// set view engine
+app.set('view engine', 'pug')
+
 // middlewares...
 app.use(logger('dev'))  // HTTP request logger.
 app.use(cookieparser())
@@ -13,13 +16,20 @@ app.use(cookieparser())
 // built-in middlewares
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public'))) // 'public' 아래에 있는 static 파일에 접근할 수 있게 된다.
+app.use(express.urlencoded({extended:false}))
 
 
 
 app.get('/', (req,res) =>{
-  res.send('Hello world!!! ')
+  // res.send('Hello world!!! ')
+  res.render('index',{title: 'Hey', message: "Hello there!"})
 
   console.log(req.cookies)
+})
+
+app.post('/', (req,res)=>{
+  console.log('post result : --- ', req)
+  res.end()
 })
 
 
