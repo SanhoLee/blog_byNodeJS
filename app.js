623db400ffb8503c7092cdbaf2043cb75e3a0094
Sanho LEE <stlsh0516@gmail.com>
2022-09-20 23:22:43 +0900

    additional work for back-end. http-server 1

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-var express = require('express')
-var app = express()
-var path = require('path')
+var express = require('express');
+var app = express();
+var path = require('path');
 var cookieparser = require('cookie-parser');
 
-var logger = require('morgan')
-const port = 4000
+var http = require('http');
+var httpServer = http.createServer(app);
+
+var logger = require('morgan');
+const port = 4000;
 
 // set view engine
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
 // middlewares...
-app.use(logger('dev'))  // HTTP request logger.
-app.use(cookieparser())
+app.use(logger('dev'));  // HTTP request logger.
+app.use(cookieparser());
 
 // built-in middlewares
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public'))) // 'public' 아래에 있는 static 파일에 접근할 수 있게 된다.
-app.use(express.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname, 'public'))); // 'public' 아래에 있는 static 파일에 접근할 수 있게 된다.
+app.use(express.urlencoded({extended:false}));
 
 
 
 app.get('/', (req,res) =>{
-  res.render('index',{title: 'Hey', message: "Hello world !!"})
+  res.render('index',{title: 'Hey', message: "Hello world !!"});
 
 })
 
@@ -38,8 +41,10 @@ app.use((err,req,res,next)=>{
   console.log('err.status : ' , err.status);
 })
 
+// app.listen(port, ()=>{
+//   console.log(`The app listening on port ${port}`);
+// })
 
-
-app.listen(port, ()=>{
-  console.log(`The app listening on port ${port}`)
-})
+httpServer.listen(port);
+httpServer.on('error', callbackFunc1);~~need to correct
+httpServer.on('listening',callbackFunc2);~~need to correct
